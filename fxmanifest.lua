5b9c309dee33156712cc5b4b7a20e9bf4d8fdef3
Schrottsoke660 <thomsenlasse@outlook.de>
2026-09-10 21:26:15 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+shared_script '@r820qs/ai_module_fg-obfuscated.lua'
+fx_version 'cerulean'
+games { 'gta5' }
+author 'Schrottsoke660'
+description 'Los Angeles Licens Plate'
+
+client_script 'plate.lua'
+file 'plates/*.png'
